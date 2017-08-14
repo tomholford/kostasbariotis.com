b2b463afce2cf5013c75c365e6f5bda3f9d8a5d3
Kostas Bariotis <konmpar@gmail.com>
2017-08-14 22:25:41 +0300

    Updates

--- path---drafts-page-0-6ae3f9a586b32a0eaed7.js
+++ path---drafts-page-0-6ae3f9a586b32a0eaed7.js
@@ -1,2 +1,2 @@
-webpackJsonp([163],{"./node_modules/json-loader/index.js!./.cache/json/drafts-page-0.json":function(e,o){e.exports={pathContext:{posts:[],page:0,pagesSum:4}}}});
+webpackJsonp([164],{"./node_modules/json-loader/index.js!./.cache/json/drafts-page-0.json":function(e,o){e.exports={pathContext:{posts:[],page:0,pagesSum:4}}}});
 //# sourceMappingURL=path---drafts-page-0-6ae3f9a586b32a0eaed7.js.map
